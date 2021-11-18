4d5be696d3578c5a43b0298986ad5455f3c7b3ee
iarigby <marvinzem@gmail.com>
2021-11-19 03:52:39 +0400

    add hw5

---
+++ hw5/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    id: 'hw5',
+    classroomName: 'დავალება 5',
+    deadline: '2021-11-21',
+    testFileName: '../../../webHwInterface/server-tests/hw5/server.js',
+    emailTemplate(){
+            return "HW2";
+    }
+}   
